6bf61065587a9eecee7e002394804661ae63b948
BZaldua <benatzaldua1775@gmail.com>
2026-06-03 09:45:06 +0200

    format: run formatter

--- tests/core/test_folder_structure.py
+++ tests/core/test_folder_structure.py
@@ -1,14 +1,16 @@
 import unittest
-from unittest.mock import MagicMock, patch
 from pathlib import Path
+from unittest.mock import MagicMock, patch
+
 from src.core import FolderStructure
+
 
 class TestFolderStructure(unittest.TestCase):
 
     def setUp(self):
-        self.patcher_config = patch('src.core.folder_structure.ConfigManager')
+        self.patcher_config = patch("src.core.folder_structure.ConfigManager")
         self.mock_config_class = self.patcher_config.start()
-        
+
         self.mock_config_instance = MagicMock()
         self.mock_config_class.return_value = self.mock_config_instance
 
@@ -18,22 +20,22 @@ class TestFolderStructure(unittest.TestCase):
             "environment": "env_dir",
             "light": "light_dir",
             "props": "prop_dir",
-            "temps": "temp_dir"
+            "temps": "temp_dir",
         }
-        
+
         def side_effect_get(key):
             if key == "asset_folders":
                 return self.mock_folders
             return f"mocked_{key.split('.')[-1]}"
 
         self.mock_config_instance.get.side_effect = side_effect_get
-        
+
         self.test_path = Path("/mock/base/path")
 
     def tearDown(self):
         self.patcher_config.stop()
 
-    @patch('pathlib.Path.mkdir')
+    @patch("pathlib.Path.mkdir")
     def test_init_and_bootstrap_folders(self, mock_mkdir):
         # Act
         fs = FolderStructure(self.test_path)
@@ -42,8 +44,8 @@ class TestFolderStructure(unittest.TestCase):
         self.assertEqual(fs.output_path, self.test_path)
         self.assertEqual(fs.folders_to_create, self.mock_folders)
         self.assertEqual(mock_mkdir.call_count, len(self.mock_folders))
-        
-    @patch('pathlib.Path.mkdir')
+
+    @patch("pathlib.Path.mkdir")
     def test_get_methods(self, mock_mkdir):
         # Act
         fs = FolderStructure(self.test_path)
@@ -56,10 +58,10 @@ class TestFolderStructure(unittest.TestCase):
         self.assertEqual(fs.get_prop(), self.test_path / "mocked_props")
         self.assertEqual(fs.get_temp(), self.test_path / "mocked_temps")
 
-    @patch('pathlib.Path.mkdir')
+    @patch("pathlib.Path.mkdir")
     def test_get_dir_names(self, mock_mkdir):
         # Act
         fs = FolderStructure(self.test_path)
-        
+
         # Assert
         self.assertEqual(fs.get_dir_names(), self.mock_folders)
